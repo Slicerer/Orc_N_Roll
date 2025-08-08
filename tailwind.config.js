18a52fea5aa4e1a9635ef36e7cca4c26973761fb
Tipouroux <perfectum@outlook.fr>
2025-08-08 04:09:13 -0400

    et acueil et recruteent premiere ebauche done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'wow-gold': '#FFB100',
+        'wow-blue': '#0081FF',
+        'wow-red': '#FF0000',
+      },
+    },
+  },
+  plugins: [],
+}
